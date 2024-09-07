5a7b870849909d8dcc63a6f0219f49180a38f9b0
ZhengranHuang <Zhengran@usf.edu>
2024-09-06 21:50:37 -0400

    Output Details

--- Coding Challenge-2.js
+++ Coding Challenge-2.js
@@ -5,3 +5,7 @@ let tip = bill >= 50 && bill <= 300 ? bill*0.15 : bill*0.2
 
 console.log("Original Bill:", bill);
 console.log("Tip:", tip);
+
+// Task2: Output Details
+
+console. log("The Original Bill was", bill, "the tip was", tip, "and the total value is", bill + tip)
