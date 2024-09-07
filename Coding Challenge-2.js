0e10e743913b9130a59814c5346fa5b024c210fd
ZhengranHuang <Zhengran@usf.edu>
2024-09-06 22:50:41 -0400

    Create and test calculateTip function

--- Coding Challenge-2.js
+++ Coding Challenge-2.js
@@ -8,4 +8,11 @@ console.log("Tip:", tip);
 
 // Task2: Output Details
 
-console. log("The Original Bill was", bill, "the tip was", tip, "and the total value is", bill + tip)
+console. log("The Original Bill was $", bill, "the tip was $", tip, "and the total value is $", bill + tip)
+
+//Task3: Create a Function
+function calcualteTip(x){
+    return x >= 50 && x <= 300 ? x*0.15 : x*0.2
+}
+
+console.log("CalcualteTip Function Output: $", calcualteTip(100));
