e42516f7b5a602fe93c170494310a804940a6988
ZhengranHuang <Zhengran@usf.edu>
2024-09-07 00:48:20 -0400

    "Implement and populate arrays for bills, tips, and totals"

--- Coding Challenge-2.js
+++ Coding Challenge-2.js
@@ -11,8 +11,22 @@ console.log("Tip:", tip);
 console. log("The Original Bill was $", bill, "the tip was $", tip, "and the total value is $", bill + tip)
 
 //Task3: Create a Function
-function calcualteTip(x){
-    return x >= 50 && x <= 300 ? x*0.15 : x*0.2
+function calcualteTip(bill){
+    return bill >= 50 && bill <= 300 ? bill*0.15 : bill*0.2
 }
 
 console.log("CalcualteTip Function Output: $", calcualteTip(100));
+
+//Task4: Utilize Arrays
+let bills = [275, 40, 430];
+const tips = bills.map(bill => calcualteTip(bill));
+
+function TotalBill(bill){
+    return bill >= 50 && bill <= 300 ? bill*0.15+bill : bill*0.2+bill
+}
+const totals = bills.map(bill => TotalBill(bill));
+
+
+console.log("bills(array)", bills);
+console.log("tips(array)", tips);
+console.log("totals(array)", totals);
